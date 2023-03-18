a6fb44253d28cf620e1d5cc16eeaf8ff247cbc58
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2023-03-17 21:46:48 -0700

    all frame scores can be entered

--- main.py
+++ main.py
@@ -4,20 +4,39 @@ import keyboard
 import time
 import math
 
+
 # Callback function that runs whenever a key is pressed down
 def retrieveKey(self, latestKeyPressRetriever):
-    #print(self.name)
     latestKeyPressRetriever["keyPressName"] = self.name
     latestKeyPressRetriever["keyJustPressed"] = True
-
-    #print(keyToSet)
-    # print(keyboard.read_key())
 
 
 # Make sure input is 0-9, x, X, /, (R to reset)
 def validateKeyPress(latestKeyPress):
-    #print('not yet implemented')
-    return True
+    if(latestKeyPress.isdigit() or 
+       latestKeyPress == "X" or 
+       latestKeyPress == "x" or 
+       latestKeyPress == "/" or
+       latestKeyPress == "R"):
+        return True
+    else: 
+        return False
+
+
+# Update the current shot in the current frame
+def updateFrameShot(currentShotScore, entireScoreBoard, nextShotNumber, frameNumber):
+    if(frameNumber == 10):
+        if(nextShotNumber == 19):
+            entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, 0)
+        elif(nextShotNumber == 20):
+            entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, 2)
+        elif(nextShotNumber == 21):
+            entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, -1)
+    else:
+        if(nextShotNumber % 2 == 1):
+            entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, 0)
+        else:
+            entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, -1)
 
 
 # Update the string for the score at the given index
@@ -29,45 +48,50 @@ def updateFrameString(oldScoreString, newValue, newValueIndex):
     print(newString)
     return newString
 
+
 # Add next shot taken onto the scoreboard in the latest shot position
-def addToScoreBoard(currentShotScore, entireScoreBoard, nextShotNumber):
-    frameNumber = math.ceil(nextShotNumber / 2)
-    if(nextShotNumber % 2 == 1):
-
-        entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, 0)
+def updateScoreBoard(currentShotScore, entireScoreBoard, nextShotNumber):
+    if(nextShotNumber > 19):
+        frameNumber = 10
     else:
-        entireScoreBoard[str(frameNumber)][0] = updateFrameString(entireScoreBoard[str(frameNumber)][0], currentShotScore, -1)
+        frameNumber = math.ceil(nextShotNumber / 2)
+    
+    updateFrameShot(currentShotScore, entireScoreBoard, nextShotNumber, frameNumber)
+
+    #calculate new score
 
 
-    #entireScoreBoard["1"][1] = currentShotScore
 
 
 # Clears the previous board and displays the updated one
 def displayBoard(entireScoreBoard):
     os.system('cls')
     print(tabulate(entireScoreBoard, headers="keys", tablefmt="fancy_grid", stralign="center"))
+    print("\n\t'r' - restart\t\t\t'esc' - quit")
+
 
 def main():
     # Passing in a dict into the on_press callback function because it's a mutable object and will update in this scope
     latestKeyPressRetriever = {"keyPressName": "", "keyJustPressed": False}
     keyboard.on_press(lambda e: retrieveKey(e, latestKeyPressRetriever))
 
-    entireScoreBoard = {"1": ["  |  ", 0], "2": ["  |  ", 10], "3": ["4 | 5", 20], "4": ["6 | 7", 40], "5": ["8 | 9", 50]}
+    entireScoreBoard = {"1": [" | ", " "], "2": [" | ", " "], "3": [" | ", " "], "4": [" | ", " "], "5": [" | ", " "], "6": [" | ", " "], "7": [" | ", " "], "8": [" | ", " "], "9": [" | ", " "], "10": [" | | ", " "]}
     nextShotNumber = 1
-
+    currentTotalScore = 0
     tabulate.PRESERVE_WHITESPACE = True
     displayBoard(entireScoreBoard)
     while(latestKeyPressRetriever["keyPressName"] != "esc"):
 
-        # Update the board if input is valid and has just been received
-        if(validateKeyPress(latestKeyPressRetriever["keyPressName"]) and latestKeyPressRetriever["keyJustPressed"]):
+        # Check if key has been pressed, only update the board if input is valid
+        if(latestKeyPressRetriever["keyJustPressed"]):
+            if(validateKeyPress(latestKeyPressRetriever["keyPressName"])):
+                updateScoreBoard(latestKeyPressRetriever["keyPressName"], entireScoreBoard, nextShotNumber)
+                nextShotNumber+=1
+                displayBoard(entireScoreBoard)
             
-            addToScoreBoard(latestKeyPressRetriever["keyPressName"], entireScoreBoard, nextShotNumber)
-            nextShotNumber+=1
-            displayBoard(entireScoreBoard)
-           
-            latestKeyPressRetriever["keyJustPressed"] = False # Set key press value to False in the retriever for it to stay upto date
-    
+            latestKeyPressRetriever["keyJustPressed"] = False
+        time.sleep(0.01) # No reason to run the while loop as fast as possible
+        
     displayBoard(entireScoreBoard)
     print("Thanks for playing!")
 
